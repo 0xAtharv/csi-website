dde7fd0effca5c8d9f58df05fc0c2d5dfa4e2134
Dibya J. Dutta <dibya12203@gmail.com>
2023-09-09 17:07:19 +0530

    [3, add navbar]

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import Logo from "../Assets/logo.png";
 
-export default function App(){
-  return(
-    <div className="text-center font-bold">start here</div>
-  )
+function NavBar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-container">
+          <a href="/#" className="nav-logo">
+            <img src={Logo} alt="" />
+          </a>
+          <div className="menu-links">
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <a href="/#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a href="/#">Events</a>
+              </li>
+              <li className="nav-item">
+                <a href="/#">About</a>
+              </li>
+              <li className="nav-item">
+                <a href="/#">Sponsership</a>
+              </li>
+              <li className="nav-item">
+                <a href="/#">Team</a>
+              </li>
+            </ul>
+          </div>
+          <div className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
+
+export default NavBar;
