d9abc0ec6617c62202fc531a30d804c8c9252779
Dibya J. Dutta <dibya12203@gmail.com>
2023-09-09 23:32:51 +0530

    navbar2.O

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,12 @@
-import React, { useState } from "react";
-import Logo from "../Assets/logo.png";
+import React from "react";
+import Navbar from "./components/Navbar"
+import "./App.css"
+import "./index.css"
 
-function NavBar() {
-  const [click, setClick] = useState(false);
-
-  const handleClick = () => setClick(!click);
-  return (
-    <>
-      <nav className="navbar">
-        <div className="nav-container">
-          <a href="/#" className="nav-logo">
-            <img src={Logo} alt="" />
-          </a>
-          <div className="menu-links">
-            <ul className={click ? "nav-menu active" : "nav-menu"}>
-              <li className="nav-item">
-                <a href="/#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a href="/#">Events</a>
-              </li>
-              <li className="nav-item">
-                <a href="/#">About</a>
-              </li>
-              <li className="nav-item">
-                <a href="/#">Sponsership</a>
-              </li>
-              <li className="nav-item">
-                <a href="/#">Team</a>
-              </li>
-            </ul>
-          </div>
-          <div className="nav-icon" onClick={handleClick}>
-            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+export default function App(){
+  return(
+    <div className="text-center font-bold">
+      <Navbar/>
+    </div>
+  )
 }
-
-export default NavBar;
