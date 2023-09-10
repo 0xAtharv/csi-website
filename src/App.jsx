070720756abe274aae2849af5c3552f89ccbb579
Prachi Vaishya <vaishyaprachi14@gmail.com>
2023-09-10 14:19:22 +0530

    timeline added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import Timeline from "./components/timeline";
 
 export default function App(){
   return(
-    <div className="text-center font-bold">start here</div>
+    // <div className="text-center font-bold">start here</div>
+    <div>
+      <Timeline />
+    </div>
   )
 }
