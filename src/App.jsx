d45d9cbd28e3025aa5d4add5770a9f420e7824af
Yashasvi <yashasvivaidya13@gmail.com>
2023-09-16 21:44:12 +0530

    code reviewed

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,49 @@
 
 
-import React from 'react'
-import data from './data1.jsx'
-import Onecard from './components/onecard.jsx'
-import Header from './components/header.jsx'
+// import React from 'react'
+// import data from './data1.jsx'
+// import Onecard from './components/onecard.jsx'
+// import Header from './components/header.jsx'
 
 
 
 
 
     
-export default function App() {
-  const Onecards = data.map(item => {
-      return (
+// export default function App() {
+//   const Onecards = data.map(item => {
+//       return (
         
-          <Onecard  
-          {...item}      
+//           <Onecard  
+//           {...item}      
           
               
-          />
-      )
-  })     
+//           />
+//       )
+//   })     
   
           
-  return (
-    <div>
-    <Header/>
-    <section className=" flex flex-wrap gap-2 px-2 lg:gap-20 md:gap-14 md:px-28 lg:px-40 bg-[#2E243C]  py-10 mx-auto">
- {Onecards}
-       </section></div>
+//   return (
+//     <div>
+//     <Header/>
+//     <section className=" flex flex-wrap gap-2 px-2 lg:gap-20 md:gap-14 md:px-28 lg:px-40 bg-[#2E243C]  py-10 mx-auto">
+//  {Onecards}
+//        </section></div>
     
     
     
-  )
-}
+//   )
+// }
+ import React from 'react'
+import Sponsors from './components/Sponsors'
+ 
+ function App() {
+   return (
+     <div>
+       <Sponsors/>
+     </div>
+   )
+ }
+ 
+ export default App
  
