810d193c8a6e928c44facb4f7701b0f9562afa6e
Anshul-Khadatkar <ajayk78932@gmail.com>
2023-09-16 12:42:43 +0530

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-import React from "react";
 
-export default function App(){
-  return(
-    <div className="text-center font-bold">start here</div>
+
+import React from 'react'
+import data from './data1.jsx'
+import Onecard from './components/onecard.jsx'
+import Header from './components/header.jsx'
+
+
+
+
+
+    
+export default function App() {
+  const Onecards = data.map(item => {
+      return (
+        
+          <Onecard  
+          {...item}      
+          
+              
+          />
+      )
+  })     
+  
+          
+  return (
+    <div>
+    <Header/>
+    <section className=" flex flex-wrap gap-2 px-2 lg:gap-20 md:gap-14 md:px-28 lg:px-40 bg-[#2E243C]  py-10 mx-auto">
+ {Onecards}
+       </section></div>
+    
+    
+    
   )
 }
+ 
