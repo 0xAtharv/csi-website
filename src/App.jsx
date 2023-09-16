f2d51cc4989deab30be079e520ef570c710f18a2
Yashasvi <yashasvivaidya13@gmail.com>
2023-09-16 13:54:32 +0530

    added gallery feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+// import Gallery from "./components/Gallery";
+// import Galleryy from "./components/galleryy";
+import Swiper from "../src/components/Swiper"
 
-export default function App(){
-  return(
-    <div className="text-center font-bold">start here
-    <div className="div">start here again</div></div>
-  )
+
+export default function App() {
+  return (
+    // <div className="text-center font-bold">
+    //   start here
+      <div >
+      {/* <Gallery /> */}
+      {/* <Galleryy/> */}
+      {/* <div className="font-Inter text-white text-2xl flex justify-center align-middle mt-20">GALLERY</div> */}
+      <Swiper/>
+      </div>
+  );
 }
