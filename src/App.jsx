c7a93aeb9e31a455555d53c88d3a16a25e833600
Prachi Vaishya <vaishyaprachi14@gmail.com>
2023-09-17 06:55:35 +0530

    changed teams section

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,12 @@
 import React from 'react'
-import Card from './components/card.jsx'
-import data from './data/data.jsx'
-
-
-
-
-
-
-  
-
-
+import WCpage from './components/WCpage.jsx'
 
 export default function App() {
-  const cards = data.map(item => {
-      return (
-          <Card
-              key={item.id}
-             {...item}
-              
-          />
-      )
-  })  
-
- 
-  
-
-        
-  
-  
           
   return (
- 
-      
-         <div>
-          <section className="flex flex-wrap gap-2 px-2 lg:gap-28 md:gap-16 md:px-10 lg:px-20 bg-[#2E243C]  py-10 ">
-         
-            {cards}
-          </section>
-      </div>
-    
-    
+    <div>
+      <WCpage />
+    </div>
   )
 }
  
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,32 +1,33 @@
-import React from 'react'
+import React from "react";
 
 export default function Card(data) {
-
-  
-    return (
-        <div className=" mx-auto  text-white md:text-lg lg:text-3xl ">
-            
-            <div className='relative  '>
-            <img src={`../public/images/${data.coverImg}`} className=" w-32 h-32 md:w-48 md:h-48 lg:w-64 lg:h-64 rounded-xl  my-6  " />
-          <div className='bg-gradient-to-tr from-neutral-900 via-[#700880]  to-neutral-900  absolute top-0 right-0   py-2 px-1 rounded-bl-md rounded-tr-md '>
-            
-         <a href={`${data.instalink}`}><img   src={`../public/images/${data.instagram}`}  className="w-4 md:w-6 lg:w-8 m-1 lg:my-[10px] md:my-[8px] my-[3px]" /></a>  
-         <a href={`${data.linkdinlink}`}><img   src={`../public/images/${data.linkdin}`}  className="w-4 md:w-6  lg:w-8  m-1 lg:my-[10px] md:my-[8px] my-[3px]" /></a>  
-          
-            
-            </div>
-            
-            
-            <div className='absolute bottom-0 rounded-b-md p-[2px] lg:p-2 md:p-[7px] lg:w-64 md:w-48 w-32  bg-gradient-to-r from-neutral-900 via-[#650873] to-[#6c27777b]  left-0'>
-            <h1 className='mx-2 '>{data.name}</h1>
-            </div>
-            
-           
-            </div>
-                        
-                        
-
+  return (
+    <div className="mx-auto text-white md:text-lg lg:text-3xl">
+      <div className="relative  ">
+        <img
+          src={`../images/${data.coverImg}`}
+          className="w-28 h-28 sm:w-36 sm:h-36 object-cover md:w-48 md:h-48 lg:w-60 lg:h-60 rounded-xl my-6"
+        />
+        
+        <div className="absolute top-0 right-0 py-2 px-1 rounded-bl-md rounded-tr-md bg-gradient-to-tr from-neutral-900 via-[#700880] to-neutral-900">
+          <a href={`${data.instalink}`}>
+            <img
+              src="../images/instagram.png"
+              className="w-4 md:w-6 lg:w-8 m-1 lg:my-2.5 md:my-2 my-2"
+            />
+          </a>
+          <a href={`${data.linkdinlink}`}>
+            <img
+              src="../images/linkedin.png"
+              className="w-4 md:w-6 lg:w-8 m-1 lg:my-2.5 md:my-2 my-2"
+            />
+          </a>
         </div>
-    )
+
+        <div className="absolute left-0 bottom-0 rounded-b-md p-1 lg:p-2 md:p-2 w-full bg-gradient-to-r from-neutral-900 via-[#650873] to-[#6c27777b]">
+          <h1 className="mx-2">{data.name}</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
-// bg-gradient-to-tr from-neutral-900 via-[#650873]  to-[#72247e76]
